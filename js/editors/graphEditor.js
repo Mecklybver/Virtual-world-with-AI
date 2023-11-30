132a281d554ab054c575420148b43f10ae6c8e96
Mecklybver <mecklybver@gmail.com>
2023-11-30 17:48:12 +0100

    first commit

---
+++ js/editors/graphEditor.js
@@ -0,0 +1,144 @@
+class GraphEditor {
+   constructor(viewport, graph) {
+     this.viewport = viewport;
+     this.canvas = viewport.canvas;
+     this.graph = graph
+     this.world = world;
+     this.snapDistance = 30
+ 
+     this.ctx = this.canvas.getContext("2d");
+ 
+     this.selected = null;
+     this.hovered = null;
+     this.dragging = false;
+     this.mouse = null;
+     this.show = true;
+ 
+     //   this.#addEventListeners();
+   }
+ 
+   enable() {
+     this.#addEventListeners();
+     this.show = true;
+   }
+   disable() {
+     this.#removeEventListeners();
+     this.selected = false;
+     this.hovered = false;
+     this.show = false;
+   }
+ 
+   #addEventListeners() {
+     this.boundMouseDown = this.#handleMouseDown.bind(this);
+     this.boundMouseMove = this.#handleMouseMove.bind(this);
+     this.boundMouseUp = () => (this.dragging = false);
+     this.boundContextMenu = (evt) => evt.preventDefault();
+     this.canvas.addEventListener("mousedown", this.boundMouseDown);
+     this.canvas.addEventListener("mousemove", this.boundMouseMove);
+     this.canvas.addEventListener("mouseup", this.boundMouseUp);
+     this.canvas.addEventListener("contextmenu", this.boundContextMenu);
+   }
+ 
+   #removeEventListeners() {
+     this.canvas.removeEventListener("mousedown", this.boundMouseDown);
+     this.canvas.removeEventListener("mousemove", this.boundMouseMove);
+     this.canvas.removeEventListener("mouseup", this.boundMouseUp);
+     // this.canvas.removeEventListener("contextmenu", this.boundContextMenu);
+   }
+ 
+   #handleMouseMove(evt) {
+     this.mouse = this.viewport.getMouse(evt, true);
+     this.hovered = getNearestPoint(
+       this.mouse,
+       this.graph.points,
+       this.snapDistance * this.viewport.zoom
+     );
+     if (this.dragging == true) {
+       this.selected.x = this.mouse.x;
+       this.selected.y = this.mouse.y;
+     }
+ 
+     const seg = getNearestSegment(
+       this.mouse,
+       this.world.graph.segments,
+       this.snapDistance * this.viewport.zoom
+     );
+     if (seg) {
+       const proj = seg.projectPoint(this.mouse);
+       if (proj.offset >= 0 && proj.offset <= 1) {
+         this.intent = proj.point;
+       } else {
+         this.intent = null;
+       }
+     } else {
+       this.intent = null;
+     }
+   }
+ 
+   #handleMouseDown(evt) {
+     if (evt.button == 2) {
+       // right click
+       if (this.selected) {
+         this.selected = null;
+       } else if (this.hovered) {
+         this.#removePoint(this.hovered);
+       }
+     }
+     if (evt.button == 0) {
+       // left click
+       if (this.hovered) {
+         this.#select(this.hovered);
+         this.dragging = true;
+         return;
+       }
+       if (this.intent ) {
+         this.#select(this.intent);
+         this.dragging = true;
+         return;
+       }
+       
+       this.graph.addPoint(this.mouse);
+       this.#select(this.mouse);
+       this.hovered = this.mouse;
+     }
+   }
+ 
+   #select(point) {
+     if (this.selected) {
+       this.graph.tryAddSegment(new Segment(this.selected, point));
+     }
+     this.selected = point;
+   }
+ 
+   #removePoint(point) {
+     this.graph.removePoint(point);
+     this.hovered = null;
+     if (this.selected == point) {
+       this.selected = null;
+     }
+   }
+ 
+   dispose() {
+     this.graph.dispose();
+     this.selected = null;
+     this.hovered = null;
+   }
+ 
+   display() {
+     if (this.show) {
+       this.graph.draw(this.ctx);
+       if (this.hovered) {
+         this.hovered.draw(this.ctx, { fill: true });
+       }
+       if (this.selected) {
+         const intent = this.hovered ? this.hovered : this.mouse;
+         new Segment(this.selected, intent).draw(ctx, { dash: [3, 3] });
+         this.selected.draw(this.ctx, { outline: true });
+       }
+       if (this.intent &&!this.hovered) {
+         this.intent.draw(this.ctx);
+       }
+     }
+   }
+ }
+ 
---
+++ js/markings/start.js
@@ -0,0 +1,67 @@
+class Start extends Marking {
+   constructor(center, directionVector, width, height, color = "blue") {
+     super(center, directionVector, width, height, color);
+     this.img = new Image();
+     this.img.src = "../car.png";
+     this.mask = document.createElement("canvas");
+     this.mask.width = width;
+     this.mask.height = height;
+     this.narrowing = 10;
+     const maskCtx = this.mask.getContext("2d");
+     this.color = color;
+     this.image = new Image();
+     this.image.src = "../car.png";
+     this.image.onload = () => {
+       maskCtx.fillStyle = color;
+       maskCtx.rect(0, 0, this.width, this.height);
+       maskCtx.fill();
+ 
+       maskCtx.globalCompositeOperation = "destination-atop";
+       maskCtx.drawImage(this.image, 0, 0, this.width, this.height);
+     };
+ 
+    
+ 
+     this.border = [
+       this.poly.segments[0],
+       this.poly.segments[1],
+       this.poly.segments[2],
+       this.poly.segments[3],
+     ];
+   }
+ 
+   draw(ctx) {
+      // for (const b of this.border) {
+      //   b.draw(ctx);
+      // }
+    
+      ctx.save();
+      ctx.translate(this.center.x, this.center.y);
+      ctx.rotate(angle(this.directionVector) - Math.PI * 0.5);
+    
+      // Draw the masked color
+      if (!this.damaged) {
+        ctx.drawImage(
+          this.mask,
+          -this.width * 0.5 + this.narrowing * 0.5,
+          -this.height + this.width-15 ,
+          this.width - this.narrowing,
+          this.height
+        );
+        ctx.globalCompositeOperation = "multiply";
+      }
+    
+      // Draw the image
+      ctx.drawImage(
+        this.img,
+        -this.width * 0.5 + this.narrowing * 0.5,
+        -this.height + this.width -15   ,
+        this.width - this.narrowing,
+        this.height 
+      );
+    
+      ctx.restore();
+    }
+    
+ }
+ 
